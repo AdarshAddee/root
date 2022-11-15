538dda9dbca29e326ca28dacda2a70471a1358e6
Adarsh Addee <59617563+AdarshAddee@users.noreply.github.com>
2022-11-15 16:50:48 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 # Sorry to here, You want to uninstall this tool!
 # We are sorry for inconvenience!
 
+clear
 echo "Sorry to here, You want to uninstall this tool!"
 echo "We are sorry for inconvenience!"
 echo "Uninstalling Root from your device"
@@ -10,7 +11,10 @@ sleep 0.5
 mv $PREFIX/bin/bash.bashrc_old $PREFIX/bin/bash.bashrc
 sleep 1
 apt-get uninstall fish ncurses-utils proot proot-distro tsu fakeroot parted -y
+sleep 0.5
 termux-reload-settings
 echo "Successfully Uninstalled Root!"
-
-
+sleep 1
+clear
+echo "Delete the root directory present in termux"
+sleep 0.5
